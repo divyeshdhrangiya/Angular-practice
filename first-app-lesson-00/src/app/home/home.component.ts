be83f06103b1bdd8f91fda909bc95ca2462d5a8f
divyesh-dhrangiya <divyeshdhrangiya8@gmail.com>
2023-05-27 12:18:42 +0530

    Add serchiing in app

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -13,13 +13,20 @@ import { HousingService } from '../housing.service';
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+      <input type="text" placeholder="Filter by city" #filter />
+      <button
+        class="primary"
+        type="button"
+        (click)="filterResults(filter.value)"
+      >
+        Search
+      </button>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
-      >
-      </app-housing-location>
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
@@ -30,5 +37,18 @@ export class HomeComponent {
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filteredLocationList: HousingLocation[] = [];
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
