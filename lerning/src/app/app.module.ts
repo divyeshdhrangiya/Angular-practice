64c81d1bfddac975329a89135c3b6e5270cf2ff8
divyesh-dhrangiya <divyeshdhrangiya8@gmail.com>
2023-05-29 14:05:22 +0530

    Create new component for seprate logic

--- lerning/src/app/app.module.ts
+++ lerning/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HeroesComponent } from './heroes/heroes.component'; // <-- NgModel lives here
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,FormsModule
