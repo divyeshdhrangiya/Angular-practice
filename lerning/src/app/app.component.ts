961d3378af232ab2482b99f2aff80465d4e517c3
divyesh-dhrangiya <divyeshdhrangiya8@gmail.com>
2023-05-29 12:05:14 +0530

    Create new component and add two way data binding

--- lerning/src/app/app.component.ts
+++ lerning/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lerning';
+  hero: Hero = {
+    id: 1,
+    name: 'divyesh',
+  };
 }
